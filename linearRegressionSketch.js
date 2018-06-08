baed84a84daae4cb9b49ec70136d7e9f3e7c99fe
Odysseuss <12751082+Odysseuss@users.noreply.github.com>
2018-06-08 10:05:16 -0400

    Initial commit

---
+++ linearRegressionSketch.js
@@ -0,0 +1,123 @@
+/*
+ * Author: Michael A. Ortiz
+ * 
+ * Linear Regression with TensorFlow.js
+ * 
+ * A derivative of the work by Daniel Shiffman in the Coding Challenge YouTube video on the
+ * CodingTrain channel found here: https://www.youtube.com/watch?v=dLp10CFIvxI
+ *  
+ * You can also find Daniel's original work here: 
+ * https://github.com/CodingTrain/website/blob/master/CodingChallenges/CC_104_tf_linear_regression/sketch.js 
+ */ 
+
+let x_vals = [];
+let y_vals = [];
+
+let w = tf.variable(tf.scalar(Math.random()), trainable=true);
+let b = tf.variable(tf.scalar(Math.random()), trainable=true); 
+const learningRate = 0.5;
+const optimizer = tf.train.sgd(learningRate);
+
+
+function setup() {
+  createCanvas(2400,2400);
+}
+
+function transformX(x) {
+  let transformedX = map(x, 0, width, 0, 1);
+  
+  return transformedX;
+}
+
+function transformY(y) {
+  let transfomredY = map(y, 0, height, 1, 0);
+
+  return transfomredY;
+}
+
+
+function inverseTransformX(x) {
+  let inverseTransformedX = map(x, 0, 1, 0, width);
+
+  return inverseTransformedX;
+}
+
+
+function inverseTransformY(y) {
+  let inverseTransformedY = map(y, 0, 1, height, 0);
+
+  return inverseTransformedY;
+}
+
+
+function transform(x, y) {
+  let transformedX = transformX(x);
+  let transformedY = transformY(y);
+
+  return [transformedX, transformedY];
+}
+
+function inverseTransform(x, y) {
+  let inverseX = inverseTransformX(x);
+  let inverseY = inverseTransformY(y);
+
+  return [inverseX, inverseY];
+}
+
+function mousePressed() {
+  createPoint(mouseX, mouseY);
+}
+
+function createPoint(mouseX, mouseY) {
+  let point = transform(mouseX, mouseY);
+  x_vals.push(point[0]);
+  y_vals.push(point[1]);
+}
+
+function mouseDragged() {
+  createPoint(mouseX, mouseY);
+}
+
+function feedForward(x) {
+  return tf.tidy(() => tf.tensor1d(x).mul(w).add(b));
+}
+
+function loss(predictions, truth) {
+  return tf.tidy(() => predictions.sub(tf.tensor1d(truth)).square().mean());
+}
+
+function draw() {
+  background(255);
+  
+  
+  stroke('red');
+  strokeWeight(8);
+  for (let i = 0; i < x_vals.length; i++) {
+    
+    point(inverseTransformX(x_vals[i]), inverseTransformY(y_vals[i]));
+  }
+
+  let costMessage = "Cost: ";
+  if (x_vals.length > 0) {
+    let cost = optimizer.minimize(() => loss(feedForward(x_vals), y_vals), true);
+    costMessage += cost.dataSync()[0];
+
+    const regressionLineYs = feedForward([0,1]);
+    ys = regressionLineYs.dataSync();
+    regressionLineYs.dispose();
+
+    stroke('black');
+    strokeWeight(5);
+    line(inverseTransformX(0), inverseTransformY(ys[0]), inverseTransformX(1), inverseTransformY(ys[1]));
+  }
+
+  stroke('black');
+  let weight = w.dataSync();
+  let bias = b.dataSync();
+  textSize(60);
+  strokeWeight(0);
+  text("Bias: " + bias[0], 1140, 742);
+  text("Weight: " + weight[0], 1140, 802);
+  text(costMessage, 1140, 862);
+  // console.log(tf.memory().numTensors);
+}
---
+++ logisticRegressionSketch.js
@@ -0,0 +1,189 @@
+/*
+ * Author: Michael A. Ortiz
+ * Logistic Regression using Tensorflow.js
+ * 
+ * Inspired by the work of Daniel Shiffman's Coding Train
+ * YouTube video here: https://www.youtube.com/watch?v=dLp10CFIvxI
+ * in which Daniel demonstrates linear regression in Tensorflow.js
+ * 
+ * In this work, I extend Daniel's idea to present a an example of using
+ * logistic regression for binary classification using Tensorflow.js.
+ */ 
+let blue_dots = [[randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0],
+                [randn_bm(), randn_bm(), 0]              
+              ];
+
+let red_dots = [[randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1],
+                [randn_bm()-0.3, randn_bm(), 1]
+              ];
+
+let w = tf.variable(tf.randomUniform([1,2], 0, 1));
+let b = tf.variable(tf.randomUniform([1,1], 0, 1)); 
+const learningRate = 1;
+const optimizer = tf.train.sgd(learningRate);
+const threshold = 0.5;
+let color = 'red';
+
+// Normal distribution between 0 and 1 using Box-Muller transform.
+// found here: https://stackoverflow.com/a/49434653
+function randn_bm() {
+  var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
+    return num;
+}
+
+function setup() {
+  createCanvas(2400,2400);
+}
+
+function transformX(x) {
+  let transformedX = map(x, 0, width, 0, 1);
+  
+  return transformedX;
+}
+
+function transformY(y) {
+  let transfomredY = map(y, 0, height, 1, 0);
+
+  return transfomredY;
+}
+
+
+function inverseTransformX(x) {
+  let inverseTransformedX = map(x, 0, 1, 0, width);
+
+  return inverseTransformedX;
+}
+
+
+function inverseTransformY(y) {
+  let inverseTransformedY = map(y, 0, 1, height, 0);
+
+  return inverseTransformedY;
+}
+
+function transform(x, y) {
+  let transformedX = transformX(x);
+  let transformedY = transformY(y);
+
+  return [transformedX, transformedY];
+}
+
+function inverseTransform(x, y) {
+  let inverseX = inverseTransformX(x);
+  let inverseY = inverseTransformY(y);
+
+  return [inverseX, inverseY];
+}
+
+function createPoint(mouseX, mouseY) {
+  let point = transform(mouseX, mouseY);
+  
+  let prediction = tf.tidy(() => feedForward(tf.tensor2d(point, [1,2])));
+  let predictionDataPromise = prediction.data();
+  predictionDataPromise.then( value => {
+    prediction.dispose();
+
+    if (value < threshold) {
+        blue_dots.push([point[0], point[1], 0]);
+    } else {
+        red_dots.push([point[0], point[1], 1]);
+    }
+  });
+}
+
+function mouseDragged() {
+  createPoint(mouseX, mouseY);
+}
+
+function mousePressed() {
+  createPoint(mouseX, mouseY);
+}
+
+function keyPressed() {
+    if (key == 'B') {
+        color = 'blue';
+    } else if (key == 'R') {
+        color = 'red';
+    }
+}
+
+function feedForward(x) {
+  let z = x.matMul(w, false, true).add(b);
+  return tf.sigmoid(z);
+}
+
+function loss(predictions, truth) {
+  return tf.tidy(() => tf.losses.logLoss(truth, predictions));
+}
+
+function drawDots(dots) {
+  for (let i = 0; i < dots.length; i++) {
+    dot = inverseTransform(dots[i][0], dots[i][1]);
+    point(dot[0], dot[1]);
+  }
+}
+
+function draw() {
+  background(255);
+  
+  stroke('blue');
+  strokeWeight(20);
+  drawDots(blue_dots);
+
+  stroke('red');
+  strokeWeight(20);
+  drawDots(red_dots);
+
+  let concat = blue_dots.concat(red_dots);
+  let costMessage = "Cost: ";
+  if (concat.length > 0) {
+    let xs = concat.map(xy => xy.slice(0,2));
+    let ys = concat.map(y => y[2]);
+    let cost = optimizer.minimize(() => loss(feedForward(tf.tensor2d(xs, [xs.length, 2])),
+                                                          tf.tensor2d(ys, [ys.length, 1])),
+                                                          true);
+    
+    costMessage += cost.dataSync()[0];
+    cost.dispose();
+  }
+  
+  let weights = w.dataSync(); 
+  let bias = b.dataSync();
+
+  stroke(0);
+  strokeWeight(5);
+  line(inverseTransformX(0),
+       inverseTransformY((-bias[0] -weights[0]*0)/(weights[1])),
+       inverseTransformX(1),
+       inverseTransformY((-bias[0] - weights[0]*1 )/ (weights[1])));
+
+  textSize(40);
+  stroke(0);
+  strokeWeight(0);
+  text("Bias: " + bias[0], 0, 682);
+  text("Weight 1: " + weights[0], 0, 742);
+  text("Weight 2: " + weights[1], 0, 802);
+  text(costMessage, 0, 862);
+  // console.log(tf.memory().numTensors);
+}
